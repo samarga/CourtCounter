f65c9c8ffdaa15fcb6b2472a866497253cb514ba
Sheila Amarga <samarga@Sheilas-MacBook-Pro.local>
2018-03-23 22:22:51 -0700

    First commit

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.android.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * This method is called when the score buttons are clicked.
+     */
+   public void threePoints(View v) {
+       scoreTeamA = scoreTeamA+3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void twoPoints(View v) {
+        scoreTeamA = scoreTeamA+2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void freeThrow(View v) {
+        scoreTeamA++;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * This method is called when the score buttons are clicked.
+     */
+    public void threePointsTeamB(View v) {
+        scoreTeamB = scoreTeamB+3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void twoPointsTeamB(View v) {
+        scoreTeamB = scoreTeamB+2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void freeThrowTeamB(View v) {
+        scoreTeamB++;
+        displayForTeamB(scoreTeamB);
+    }
+
+
+    public void resetScore(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+
+
+}
